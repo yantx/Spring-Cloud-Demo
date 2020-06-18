4377f96cd9d2ea3c84089aa43845d73ae549025d
yantx <yantxd@qq.com>
2020-06-18 10:59:47 +0800

    配置中心服务端工程

---
+++ jerry-config-single-server/src/main/java/com/jerry/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.jerry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author ：yantx
+ * @version ： 1.0
+ * @date ：2020/6/17 14:12
+ * @description：
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class,args);
+    }
+}
